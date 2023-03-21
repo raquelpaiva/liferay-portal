4bbc952d9efae88ddc85631f7cf9354b68553668
raquel.paiva <raquelpaiva15@hotmail.com>
2023-03-21 14:47:33 -0300

    layout adjustments

--- modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/raylife-ap-fragments/fragments/avatar-dropdown-menu/index.js
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/raylife-ap-fragments/fragments/avatar-dropdown-menu/index.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 const avatarElement = document.querySelector('.applications-menu-header span');
 const left = avatarElement.offsetLeft;
 const height = avatarElement.offsetHeight;
@@ -5,7 +19,12 @@ const height = avatarElement.offsetHeight;
 const dropdown = document.querySelector('.dropdown.applications-menu-wrapper');
 dropdown.onclick = () => dropdown.classList.toggle('show');
 
-const dropdownItems = document.querySelector('.dropdown-items');
+// const dropdownItems = document.querySelector('.dropdown-items');
+
+const userId = Liferay.ThemeDisplay.getUserName();
+document.querySelector('.user-name').innerHTML = userId;
+
+// console.log('user', userId);
 
 const dropdownContent = document.querySelector('.dropdown-content');
 dropdownContent.style.left = `${left + 10}px`;
@@ -20,12 +39,14 @@ const redirectUrl = (routeName) => {
 	window.location.href = `${origin}${siteName}/${routeName}`;
 };
 
-const redirectUrlWithoutAp = (portletId) => {
-	const token = Liferay.authToken;
-	const routeconfig = '&p_p_lifecycle=0&p_p_auth=';
+// console.log(Liferay.ThemeDisplay);
 
-	window.location.href = `${portletId}${routeconfig}${token}`;
-};
+// const redirectUrlWithoutAp = (portletId) => {
+// 	const token = Liferay.authToken;
+// 	const routeconfig = '&p_p_lifecycle=0&p_p_auth=';
+
+// 	window.location.href = `${portletId}${routeconfig}${token}`;
+// };
 
 const btnDashboard = document.getElementById('dropdown-item-dashboard');
 const btnMyaccount = document.getElementById('dropdown-item-myaccount');
@@ -35,8 +56,12 @@ const btnAccountsettings = document.getElementById(
 );
 const btnSignout = document.getElementById('dropdown-item-signout');
 
+const myAcc = (window.location.href = btnMyaccount.href);
+
+const mySignO = (window.location.href = btnSignout.href);
+
 btnDashboard.onclick = () => redirectUrl('dashboard');
-btnMyaccount.onclick = () => (window.location.href = btnMyaccount.href);
+btnMyaccount.onclick = () => myAcc;
 btnNotifications.onclick = () => redirectUrl('notifications-list');
 btnAccountsettings.onclick = () => redirectUrl('account-settings');
-btnSignout.onclick = () => (window.location.href = btnSignout.href);
+btnSignout.onclick = () => mySignO;
